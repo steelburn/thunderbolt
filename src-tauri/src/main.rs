9f35dae6a112281e9785e0f79a5940be45ab3d58
Chris Roth <chris@cjroth.com>
2025-02-25 12:34:44 -0500

    run db on start

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,25 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod db;
 mod embedding;
 mod imap_client;
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<()> {
     // Handle the Result and Option types
     match imap_client::fetch_inbox_top() {
         Ok(Some(body)) => println!("{}", body),
         Ok(None) => println!("No message found"),
         Err(e) => eprintln!("Error: {}", e),
     }
-    mozilla_assist_lib::run()
+
+    match db::init_db().await {
+        Ok(_db) => println!("Database initialized"),
+        Err(e) => eprintln!("Error: {}", e),
+    }
+
+    mozilla_assist_lib::run();
+
+    Ok(())
 }
